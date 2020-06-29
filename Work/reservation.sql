7a68395c5c068a76534816e9393438585f0bea01
Your Name <you@example.com>
2020-06-29 14:42:40 +0100

    fichier sql

--- Work/reservation.sql
+++ Work/reservation.sql
@@ -1,55 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jun 29, 2020 at 01:40 PM
+-- Server version: 5.7.28
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `reservation`
+-- Database: `reservation`
 --
 
 -- --------------------------------------------------------
-CREATE DATABASE reservation;
-use reservation;
 
 --
--- Structure de la table `admin`
+-- Table structure for table `admin`
 --
 
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) ,
-  `login` varchar(30),
-  `password` varchar(30),
+  `id` int(11) NOT NULL,
+  `login` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert into admin (id,login,password) values (1,'admin','admin');
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `password`) VALUES
+(1, 'admin', 'admin');
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `etudiant`
---
-
---
--- Structure de la table `chambre`
+-- Table structure for table `chambre`
 --
 
 DROP TABLE IF EXISTS `chambre`;
 CREATE TABLE IF NOT EXISTS `chambre` (
   `numero` varchar(50) NOT NULL,
   `numerobat` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL,
+  `typ` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`numero`)
-) ENGINE=InnoDB ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `chambre`
+-- Dumping data for table `chambre`
 --
 
-INSERT INTO `chambre` (`numero`, `numerobat`, `type`) VALUES
+INSERT INTO `chambre` (`numero`, `numerobat`, `typ`) VALUES
 ('001', 1, 'individuel'),
 ('002', 2, 'deux'),
-('003', 3, 'individuel');
+('003', 3, 'individuel'),
+('004', 2, 'individuel');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `etudiant`
+-- Table structure for table `etudiant`
 --
 
 DROP TABLE IF EXISTS `etudiant`;
@@ -60,14 +81,18 @@ CREATE TABLE IF NOT EXISTS `etudiant` (
   `email` varchar(250) NOT NULL,
   `telephone` int(11) NOT NULL,
   `date_de_naissance` date NOT NULL
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `etudiant`
+-- Dumping data for table `etudiant`
 --
 
 INSERT INTO `etudiant` (`matricule`, `nom`, `prenom`, `email`, `telephone`, `date_de_naissance`) VALUES
-('2018 et nt 0001', 'etudiant', 'etudiant', 'etudiant@gmail.com', 845678484, '2000-06-16');
-INSERT INTO `etudiant` (`matricule`, `nom`, `prenom`, `email`, `telephone`, `date_de_naissance`) VALUES
-('2018nt0002', 'etudiant1', 'etudiant1', 'etudiant1@gmail.com', 849678484, '2000-06-17');
+('2018fasa0002', 'diop', 'Fall', 'sdsdcv14@gmail.com', 7825221, '2000-04-05'),
+('2018fasa0001', 'diop', 'Fall', 'fallsyfh@gmail.com', 7825221, '2000-06-15'),
+('2018etet0003', 'diop', 'gueye', 'gueyedou@gmail.com', 78252215, '2000-06-15');
+COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
